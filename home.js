12401b0922ff97b02d79a4d8a234b502f0449f80
Patrick <patrickrice20@gmail.com>
2016-09-18 12:02:54 -0700

    second commit

---
+++ home.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    document.getElementById("myBtn").addEventListener("click", function () {
+        document.getElementById("demo").innerHTML = "Hello World";
+    });
+});
+function myFunction(x) {
+    x.classList.toggle("change");
+  };
